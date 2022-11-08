0bc25e2a83d2aa92d115e9ed7a41d270cba274e6
dannycastillotecsup <danny.castillo@tecsup.edu.pe>
2022-11-08 15:43:18 -0500

    Nuevos cambios

--- src/main/java/com/tecsup/petclinic/services/OwnerService.java
+++ src/main/java/com/tecsup/petclinic/services/OwnerService.java
@@ -7,64 +7,64 @@ import com.tecsup.petclinic.exception.OwnerNotFoundException;
 
 public interface OwnerService {
 
-	Owner create(Owner owner);
+    Owner create(Owner owner);
 
-	/**
-	 * 
-	 * @param owner
-	 * @return
-	 */
-	Owner update(Owner owner);
+    /**
+     * 
+     * @param owner
+     * @return
+     */
+    Owner update(Owner owner);
 
-	/**
-	 * 
-	 * @param id
-	 * @throws OwnerNotFoundException
-	 */
-	void delete(Long id) throws OwnerNotFoundException;
+    /**
+     * 
+     * @param id
+     * @throws OwnerNotFoundException
+     */
+    void delete(Long id) throws OwnerNotFoundException;
 
-	/**
-	 * 
-	 * @param id
-	 * @return
-	 */
-	Owner findById(long id) throws OwnerNotFoundException;
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    Owner findById(long id) throws OwnerNotFoundException;
 
-	/**
-	 * 
-	 * @param name
-	 * @return
-	 */
-	List<Owner> findByName(String first_name);
+    /**
+     * 
+     * @param name
+     * @return
+     */
+    List<Owner> findByfirst_name(String first_name);
 
-	/**
-	 * 
-	 * @param typeId
-	 * @return
-	 */
-	List<Owner> findByLastName(String last_name);
+    /**
+     * 
+     * @param typeId
+     * @return
+     */
+    List<Owner> findBylast_name(String last_name);
 
-	/**
-	 * 
-	 * @return
-	 */
-	List<Owner> findByAdress(String adress);
+    /**
+     * 
+     * @return
+     */
+    List<Owner> findByaddress(String adress);
 
-	/**
-	 * 
-	 * @return
-	 */
-	List<Owner> findByCity(String city);
+    /**
+     * 
+     * @return
+     */
+    List<Owner> findBycity(String city);
 
-	/**
-	 * 
-	 * @return
-	 */
-	List<Owner> findByTelephone(String telephone);
+    /**
+     * 
+     * @return
+     */
+    List<Owner> findBytelephone(String telephone);
 
-	/**
-	 * 
-	 * @return
-	 */
-	Iterable<Owner> findAll();
+    /**
+     * 
+     * @return
+     */
+    Iterable<Owner> findAll();
 }
